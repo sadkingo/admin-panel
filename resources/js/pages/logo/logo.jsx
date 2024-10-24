538f12087d53f7694002bf9331f075a0c809849f
SaDKinG <hker0004@gmail.com>
2024-10-24 14:13:21 +0100

    Added Dynamic Logo component

---
+++ resources/js/pages/logo/logo.jsx
@@ -0,0 +1,16 @@
+import "./logo.style.css";
+
+export default function Logo({ scale }) {
+    const height = 54.3 * scale;
+    const width = 7.7 * scale;
+    const style = {
+        height: height + "px",
+        width: width + "px",
+    }
+    return <div style={{ height: height + 3.2 * scale, width: height + 3.2 * scale}} className="logo">
+        <div style={style} className="top-right rectangle"></div>
+        <div style={style} className="bottom-right rectangle"></div>
+        <div style={style} className="bottom-left rectangle"></div>
+        <div style={style} className="top-left rectangle"></div>
+    </div>
+}
