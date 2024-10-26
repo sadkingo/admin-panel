8d2a895c6a3fe032b831296a66bb00c12dd8a4ca
SaDKinG <hker0004@gmail.com>
2024-10-26 12:53:14 +0100

    Added Login logic

--- resources/js/pages/login/login.jsx
+++ resources/js/pages/login/login.jsx
@@ -1,5 +1,6 @@
 import "./login.style.css";
 import Circle from "./circle";
+import Logo from "../logo/logo"
 export default function Login() {
     const circleProps = [
         { bottom: -40, left: -182, diameter: 590 },
@@ -8,7 +9,24 @@ export default function Login() {
         { bottom: -354, left: -521, diameter: 1514 },
     ];
     return <>
-        <div className="left-content"></div>
+        <div className="left-content">
+            <div className="head-logo">
+                <Logo scale={0.5} />
+                <p>Hope Ui</p>
+            </div>
+            <h1>Sign In</h1>
+            <h4>Sign in to stay connected.</h4>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" placeholder="example@example.com" />
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+                <input type="checkbox" name="remember" id="remember" />
+                <label htmlFor="remember">Remember me?</label>
+                <a href="#">Forgot Password</a>
+                <button type="submit">Sign in</button>
+            </form>
+        </div>
         <div className="right-content circle-parent">
             {circleProps.map((circleProp, index) =>
                 <Circle
