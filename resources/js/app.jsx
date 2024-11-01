3b593445f0654141e03aaff78a34f4c24479fb4a
SaDKinG <hker0004@gmail.com>
2024-11-01 16:01:47 +0100

    Added react router

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDom from "react-dom/client"
 import Login from "./pages/login/login";
-function App() {
-    return <Login>
-    </Login>
+import SingUp from "./pages/sing-up/sing-up";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([{
+    path: "/login",
+    element: <Login />
+}, {
+    path: "/sign-up",
+    element: <SingUp />
 }
-
-ReactDom.createRoot(document.getElementById("app")).render(<App />)
+])
+ReactDom.createRoot(document.getElementById("app")).render(<RouterProvider router={router} />)
