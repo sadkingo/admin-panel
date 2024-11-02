a403d11aba4b5784588ea43d6afff230e02d9350
SaDKinG <hker0004@gmail.com>
2024-11-02 12:23:01 +0100

    Added  singUp page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom/client"
 import Login from "./pages/login/login";
-import SingUp from "./pages/sing-up/sing-up";
+import SingUp from "./pages/sing-up/singUp";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([{
     path: "/login",
---
+++ resources/js/pages/sing-up/singUp.jsx
@@ -0,0 +1,72 @@
+import "./singUp.style.css";
+import { Link } from "react-router-dom";
+import Circle from "../../common/circle"
+import * as Ui from "../../common"
+import facebookLogo from "../../../images/logos/logos--facebook.svg"
+import googleLogo from "../../../images/logos/logos--google-icon.svg"
+import instagramLogo from "../../../images/logos/logos--instagram.svg"
+import LinkedLogo from "../../../images/logos/logos--linkedin.svg"
+export default function SingUp() {
+    const circleProps = [
+        { top: -40, right: -182, diameter: 593 },
+        { top: -160, right: -240, diameter: 746 },
+        { top: -318, right: -286, diameter: 996 },
+        { top: -350, right: -354, diameter: 1172 },
+        { top: -522, right: -354, diameter: 1514 },
+    ];
+    return <>
+        <div className="sub-content circle-parent">
+            {circleProps.map((circleProp, index) =>
+                <Circle
+                    key={index}
+                    {...circleProp}
+                ></Circle>)}
+        </div>
+        <div className="main-content">
+            <div className="head-logo">
+                <Ui.Logo scale={0.5} />
+                <p>Hope Ui</p>
+            </div>
+            <Ui.Form title="Sign Up" subtitle="Create your Hope UI account.">
+                <div className="input-group">
+                    <Ui.Label htmlFor={"first-name"}>First Name</Ui.Label>
+                    <Ui.Input type="text" name="first_name" id="first-name"/>
+                </div>
+                <div className="input-group">
+                    <Ui.Label htmlFor="last-name">Last Name</Ui.Label>
+                    <Ui.Input type="text" name="last_name" id="last-name"/>
+                </div>
+                <div className="input-group">
+                    <Ui.Label htmlFor={"email"}>Email</Ui.Label>
+                    <Ui.Input type="email" name="email" id="email" placeholder="example@example.com" />
+                </div>
+                <div className="input-group">
+                    <Ui.Label htmlFor={"tel"}>Phone No.</Ui.Label>
+                    <Ui.Input type="tel" name="tel" id="tel" placeholder="+213" />
+                </div>
+                <div className="input-group">
+                    <Ui.Label htmlFor={"password"}>Password</Ui.Label>
+                    <Ui.Input type="password" name="password" id="password" />
+                </div>
+                <div className="input-group">
+                    <Ui.Label htmlFor={"passwordConfirmation"}>Confirm password</Ui.Label>
+                    <Ui.Input type="password" name="password_confirmation" id="passwordConfirmation" />
+                </div>
+                <div className="custom-form center-elements">
+                    <Ui.Input type="checkbox" name="remember" id="remember" />
+                    <Ui.Label htmlFor={"remember"} >I agree with the terms of use</Ui.Label>
+                </div>
+                <Ui.Button>Sign in</Ui.Button>
+            </Ui.Form>
+            <p>or sign up with other accounts?</p>
+            <div className="social-media-logos">
+                <a href="#"><img src={googleLogo} alt="linkedin" /></a>
+                <a href="#"><img src={facebookLogo} alt="linkedin" /></a>
+                <a href="#"><img src={LinkedLogo} alt="linkedin" /></a>
+                <a href="#"><img src={instagramLogo} alt="linkedin" /></a>
+            </div>
+            <p>Already have an Account <Link to="/login">Sign in</Link></p>
+        </div>
+    </>
+}
+
