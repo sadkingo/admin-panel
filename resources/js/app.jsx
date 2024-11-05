efd3715c5100fe2bad7e61e50b37180dec875a8c
SaDKinG <hker0004@gmail.com>
2024-11-05 02:11:25 +0100

    Added dynamic 404 page

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import ReactDom from "react-dom/client"
 import Login from "./pages/login/login";
 import SingUp from "./pages/sing-up/singUp";
+import Error from "./pages/error/error";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const router = createBrowserRouter([{
     path: "/login",
     element: <Login />
 }, {
+    path: "/",
+    element: <Login />
+}, {
     path: "/sign-up",
     element: <SingUp />
+}, {
+    path: "*",
+    element: <Error
+        errorNumber={404}
+        errorTitle="Oops! This Page is Not Found."
+        errorSubtitle="The requested page dose not exist"
+    />
 }
 ])
 ReactDom.createRoot(document.getElementById("app")).render(<RouterProvider router={router} />)
---
+++ resources/js/pages/error/error.jsx
@@ -0,0 +1,27 @@
+import "./error.style.css";
+import Circle from "../../common/circle";
+import { Link } from "react-router-dom";
+
+export default function Error({ errorNumber, errorTitle, errorSubtitle }) {
+    const circleProps = [
+        { id: 0, diameter: 938, top: "50%" },
+        { id: 0, diameter: 1224, top: "50%" },
+        { id: 0, diameter: 1430, top: "50%" },
+        { id: 0, diameter: 1655, top: "50%" },
+        { id: 0, diameter: 1900, top: "50%" },
+    ]
+    return (<main>
+        <div className="error-number">{errorNumber}</div>
+        {circleProps.map(circleProp =>
+            <Circle className="circle-mod"
+                key={circleProp.id}
+                {...circleProp}
+            />)}
+        <div className="container" >
+            <p className="title">{errorTitle}</p>
+            <p className="subtitle">{errorSubtitle}</p>
+            <Link to="/"><button className="back-btn">Back to Home</button></Link>
+        </div>
+    </main>
+    );
+}
