35d4be3bf56f32fb88ff2fa903d3544a75c5c48f
SaDKinG <hker0004@gmail.com>
2024-11-11 00:01:03 +0100

    Changed to BEM naming

--- resources/js/pages/error/error.jsx
+++ resources/js/pages/error/error.jsx
@@ -10,18 +10,19 @@ export default function Error({ errorNumber, errorTitle, errorSubtitle }) {
         { id: 3, diameter: 1655, top: "50%" },
         { id: 4, diameter: 1900, top: "50%" },
     ]
-    return (<main className="error-page">
-        <div className="error-number">{errorNumber}</div>
-        {circleProps.map(circleProp =>
-            <Circle className="error-page-circle"
-                key={circleProp.id}
-                {...circleProp}
-            />)}
-        <div className="container" >
-            <p className="title">{errorTitle}</p>
-            <p className="subtitle">{errorSubtitle}</p>
-            <Link to="/"><button className="back-btn">Back to Home</button></Link>
-        </div>
-    </main>
+    return (
+        <main className="error-page">
+            <div className="error-page__number">{errorNumber}</div>
+            {circleProps.map(circleProp =>
+                <Circle className="error-page__circle" key={circleProp.id} {...circleProp} />
+            )}
+            <div className="error-page__container">
+                <p className="error-page__title">{errorTitle}</p>
+                <p className="error-page__subtitle">{errorSubtitle}</p>
+                <Link to="/">
+                    <button className="error-page__back-btn">Back to Home</button>
+                </Link>
+            </div>
+        </main>
     );
 }
