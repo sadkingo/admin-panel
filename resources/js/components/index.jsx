51bcb2b62c3e548d5f05480d36b08c2e082cb5b1
sadkingo <hker0004@gmail.com>
2024-12-04 17:32:56 +0100

    Removed In the refactor

--- resources/js/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as Logo } from './logo/Logo';
-export { default as Form } from './form/form';
-export { default as Label } from './form/label';
-export { default as Input } from './form/input';
-export { default as Button } from './form/button';
